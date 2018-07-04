9bed2d633b3bf080a09d6a9af95b9f57fac1a518
Ibnu Jakaria <ibnujakaria@outlook.com>
2018-07-04 11:13:24 +0700

    display kritik dan saran only for admin

--- src/components/frontend/KritikDanSaranIndex.vue
+++ src/components/frontend/KritikDanSaranIndex.vue
@@ -8,15 +8,25 @@
           <li><router-link to="#">Settings</router-link></li>
           <li><router-link to="#">Ganti Password</router-link></li>
         </ol>
-        <div class="panel panel-default loading-container">
-          <div class="panel-body">
-            <div class="alert alert-danger" v-if="error">
-              <strong>Gagal</strong> <br>
-              {{error}}
-            </div>
-            <div class="alert alert-success" v-if="success">
-              <strong>Sukses</strong> <br>
-              {{success}}
+        <div v-for="krisar of listKrisar">
+          <div class="panel panel-default loading-container">
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-1">
+                  <img :src="krisar.mahasiswa.foto.foto_src" v-if="krisar.mahasiswa.foto">
+                  <img src="http://via.placeholder.com/350x350" v-else>
+                </div>
+                <div class="col-md-11">
+                  <h4>
+                    {{ krisar.mahasiswa.nama }} 
+                    <small class="text-muted">{{ krisar.mahasiswa.nim }}</small>
+                  </h4>
+                  <small style="float: right" class="text-muted">{{ krisar.created_at_for_humans }}</small>
+                  <div>
+                    <p>{{ krisar.isi_krisar }}</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -27,6 +37,11 @@
 
 <script type="text/javascript">
 export default {
+  data () {
+    return {
+      listKrisar: []
+    }
+  },
   mounted () {
     if (this.$store.state.auth.role !== 'admin') {
       this.$router.push('/404')
@@ -35,7 +50,26 @@ export default {
     this.getSemuaKritikDanSaran()
   },
   methods: {
-    getSemuaKritikDanSaran () {}
+    async getSemuaKritikDanSaran () {
+      let token = this.$store.getters.token
+
+      try {
+        let { data } = await this.$http.get(`mahasiswa/krisar?api_token=${token}`)
+        this.listKrisar = data.message
+      } catch (error) {
+        alert('Oops. Something error occured')
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+    object-position: top;
+  }
+</style>
