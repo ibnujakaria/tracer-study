f842b303d8175bb241060af39eb264d761f9f461
Ibnu Jakaria <ibnujakaria@outlook.com>
2018-11-12 14:02:58 +0700

    change default port to 80

--- build/server.js
+++ build/server.js
@@ -1,11 +1,9 @@
 // server.js
 var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
 
 app = express();
 app.use(express.static('dist'))
 
-var port = process.env.PORT || 5000;
+var port = 80;
 app.listen(port);
 console.log('server started '+ port);
